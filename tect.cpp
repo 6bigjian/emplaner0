36844c641f684d977c4ac9e26161d7891472139d
6bigjian <1262754623@qq.com>
2022-07-12 10:45:52 +0800

    ...

--- tect.cpp
+++ tect.cpp
@@ -17,14 +17,4 @@ int main()
     return 0;
 
     return 0;
-    return 1;
-    return 1;
-    return 1;
-    return 1;
-    return 1;
-    return 1;
-    return 1;
-    return 1;
-    return 1;
-    return 1;
 }   
